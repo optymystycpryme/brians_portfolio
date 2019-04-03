deebc64804c6648afba7c00498f19cf2b2fed3ac
brian haltom <brianwh87@gmail.com>
2019-04-02 18:14:32 -0700

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -13,29 +13,29 @@
 ActiveRecord::Schema.define(version: 2019_04_02_222741) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'blogs', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "portfolios", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_img"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'portfolios', force: :cascade do |t|
+    t.string 'title'
+    t.string 'subtitle'
+    t.text 'body'
+    t.text 'main_image'
+    t.text 'thumb_img'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string "title"
-    t.integer "percent_utilized"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'skills', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'percent_utilized'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: 'Pellentesque sit amet eros mi. Sed elementum arcu et elit fringilla, eu accumsan magna laoreet. Etiam a consequat neque. Donec pellentesque vel enim sed auctor. Fusce hendrerit ipsum eu tempus suscipit. Etiam luctus dui et porttitor pellentesque. Aliquam ut sapien eget eros suscipit eleifend ac ac sapien. Nunc ac tempor purus, commodo semper odio.'
+  )
+end
+
+puts '10 blog posts created'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: 'My Great Service',
+    body: 'Duis nec metus blandit, elementum purus at, fermentum eros. Quisque vitae auctor arcu. Aliquam rhoncus in sem dapibus laoreet. ',
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_img: 'https://via.placeholder.com/350x200'
+  )
+end
+
+puts '9 portfolio items created'
