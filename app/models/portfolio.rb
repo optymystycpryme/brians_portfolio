8d876e52208d2b05ff6d88c1b027968ff75ae849
brian haltom <brianwh87@gmail.com>
2019-04-03 20:53:53 -0700

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,11 @@
 # Portfolio Model
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_img
+
+  # Creating custom scope... Awesome!!!
+  def self.react
+    where(subtitle: 'React')
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: 'Pellentesque sit amet eros mi. Sed elementum arcu et elit fringilla, eu accumsan magna laoreet. Etiam a consequat neque. Donec pellentesque vel enim sed auctor. Fusce hendrerit ipsum eu tempus suscipit. Etiam luctus dui et porttitor pellentesque. Aliquam ut sapien eget eros suscipit eleifend ac ac sapien. Nunc ac tempor purus, commodo semper odio.'
+    body: 'Pellentesque sit amet eros mi. Sed elementum arcu et elit fringilla, eu accumsan magna laoreet. Etiam a consequat neque. Donec pellentesque vel enim sed auctor. Fusce hendrerit ipsum eu tempus suscipit. Etiam luctus dui et porttitor pellentesque. Aliquam ut sapien eget eros suscipit eleifend ac ac sapien. Nunc ac tempor purus, commodo semper odio.',
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts '5 skills created'
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: 'My Great Service',
+    subtitle: 'Ruby on Rails',
+    body: 'Duis nec metus blandit, elementum purus at, fermentum eros. Quisque vitae auctor arcu. Aliquam rhoncus in sem dapibus laoreet.',
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_img: 'https://via.placeholder.com/350x200'
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: 'React',
     body: 'Duis nec metus blandit, elementum purus at, fermentum eros. Quisque vitae auctor arcu. Aliquam rhoncus in sem dapibus laoreet.',
     main_image: 'https://via.placeholder.com/600x400',
     thumb_img: 'https://via.placeholder.com/350x200'
