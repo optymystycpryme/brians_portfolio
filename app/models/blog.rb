d825d211716387ba93fdae2ca6211ebf2b76bb6f
brian haltom <brianwh87@gmail.com>
2019-04-03 17:20:11 -0700

    Integrated database relationship between topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,5 +4,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :blog
+  validates_presence_of :title, :body
+
+  belongs_to :topic
 end
