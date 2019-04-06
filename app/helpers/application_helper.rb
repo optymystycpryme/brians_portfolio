61568ea42d3544cf2eb6563c6dfd3b9521b3e9e9
brian haltom <brianwh87@gmail.com>
2019-04-05 20:13:51 -0700

    Ingtegrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def copyright_generator
+    PrymeCreationViewTool::Renderer.copyright 'PrymeCreation', 'All rights reserved'
+  end
 end
